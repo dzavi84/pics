796f03cd7a4dcce6a62c6d963c21df973e429301
dzavi84 <milanvidanovic@yahoo.com>
2021-12-20 20:04:38 +0100

    commitlast

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
-class SearchBar extends React.Component{
-    render(){
-        return <div>Search Bar</div>
-    }
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div className='ui segment'>
+        <form className='ui form'>
+          <div className='field'>
+            <label>Image Search</label>
+            <input text='text' />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
