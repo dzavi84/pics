794c73ecfbd0a42755cb89494cb19d7304f8ed72
dzavi84 <milanvidanovic@yahoo.com>
2021-12-23 11:51:35 +0100

    commit2

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios
+      .get('https://api.unsplash.com/search/photos', {
+        params: { query: term },
+        headers: {
+          Authorization:
+            'Client-ID RwaTqozcmx6QZlEUDEXqvEOi4BPXYoHJBpP9-CvZBIo',
+        },
+      })
+      .then((response) => {
+        console.log(response.data.results);
+      });
   }
 
   render() {
